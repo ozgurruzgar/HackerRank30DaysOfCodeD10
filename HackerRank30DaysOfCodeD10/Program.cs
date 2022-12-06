1725be99606a4c72e436828098c272d94526cb82
Özgür <zgr@DESKTOP-1NLBACI>
2022-12-06 05:37:01 +0300

    Add project files.

---
+++ HackerRank30DaysOfCodeD10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HackerRank30DaysOfCodeD10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            var reminder = 0;
+            var count = 0;
+            var max = 0;
+
+            while (n > 0)
+            {
+                reminder = n % 2;
+                n /= 2;
+
+                if (reminder == 1)
+                {
+                    count++;
+                    max = Math.Max(max, count);
+                }
+                else
+                {
+                    count = 0;
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
